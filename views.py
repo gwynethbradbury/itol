0f1a8807f2eaf51789047a27d29caa07febefa66
cenv0594 <gwyneth.bradbury@ouce.ox.ac.uk>
2018-02-06 15:35:07 +0000

    -

--- views.py
+++ views.py
@@ -142,7 +142,7 @@ def drafts():
     return render_template('index.html',object_list=query.all(),
         check_bounds=False)
 
-@app.route(app_path+'<slug>/')
+@app.route('/<slug>/')
 def detail(slug):
     if session.get('logged_in'):
         # query = models.Entry.query.all()
